5e2f5e3748f1e752f9b725e2a67ac73a244250bf
Jonas Sjöberg <jonas@seaberg.se>
2008-11-12 20:48:01 +0100

    Some minor cleanups in unit_converter_controller, changed some comments.

--- app/controllers/unit_converter_controller.rb
+++ app/controllers/unit_converter_controller.rb
@@ -1,5 +1,6 @@
 # TODO: Perhaps it is possible to use automatic loading instead, if the unit_converter conforms to the
 # standard naming convention using modules, UnitConverter::UnitConverter or similar.
+# This error will disappear when the unit_converter library is converted to a proper ruby gem
 require File.expand_path(File.dirname(__FILE__) + "/../../lib/unit_converter/unit_converter.rb")
 
 class UnitConverterController < ApplicationController
@@ -14,12 +15,10 @@ class UnitConverterController < ApplicationController
     end
   end
 
-  # TODO: Refactor unit_converter parameter? Perhaps make it accessible from all methods in controller instead
   def perform_conversion()
     uc = UnitConverter.new
     @results = nil
     
-    # flash[:last_input] = params[:input] # Save the input so it can be entered as default value for new conversions
     begin
       input = parse_input(params[:input])
       @unit = input[:unit]
@@ -32,8 +31,6 @@ class UnitConverterController < ApplicationController
   end    
   
   def autocomplete_conversion_input
-    logger.info("CONVERSION_INPUT: " + params[:conversion][:input]) # DEBUG
-    
     re = Regexp.new("^#{params[:conversion][:input]}", "i")    
     uc = UnitConverter.new
     units = uc.get_units
@@ -44,14 +41,7 @@ class UnitConverterController < ApplicationController
   end
   
   private
-  
-  # Probably obsolete... unless I want to add non-javascript support
-  def redirect_to_index(msg = nil, last_input = nil)
-    flash[:notice] = msg
-    flash[:last_input] = last_input
-    redirect_to :action => :index
-  end  
-  
+    
   # Parse input into a hash containing :value => and :unit => unit
   # TODO: characters in middle of a conversion value does not produce an error, for example: 1223qwee2 Pound
   # TODO: Should be possible to begin a value with a decimal sign, for example: .72 Pound should be a valid conversion
