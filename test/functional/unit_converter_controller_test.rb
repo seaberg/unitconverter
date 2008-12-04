84c0e678baaf64fce9cde22001938263a09f2fd2
Jonas Sjöberg <jonas@seaberg.se>
2008-12-04 17:25:50 +0100

    Fixed unit names in unit_converter_controller_test.rb

--- test/functional/unit_converter_controller_test.rb
+++ test/functional/unit_converter_controller_test.rb
@@ -18,36 +18,36 @@ class UnitConverterControllerTest < Test::Unit::TestCase
   end
 
   def test_length_conversion
-    post :perform_conversion, :input => "27.154 m"
+    post :perform_conversion, :input => "27.154 Meters"
     results = assigns["results"]
-    assert_not_nil results, "Conversion with input: \"27.154 m\" failed"
-    assert_in_delta(0.027154, results["Km"], 0.01)
-    assert_in_delta(0.016872713, results["Mile"], 0.01)
-    assert_in_delta(29.695975, results["Yard"], 0.01)
-    assert_in_delta(89.087927, results["Foot"], 0.01)
-    assert_in_delta(1069.055, results["Inch"], 0.01)
-    assert_in_delta(2715.4, results["Centimeter"], 0.01)
-    assert_in_delta(27154, results["Millimeter"], 0.01)
-    assert_in_delta(0.014661987, results["Nautical Mile"], 0.01)
+    assert_not_nil results, "Conversion with input: \"27.154 Meters\" failed"
+    assert_in_delta(0.027154, results["Kilometers"], 0.01)
+    assert_in_delta(0.016872713, results["Miles"], 0.01)
+    assert_in_delta(29.695975, results["Yards"], 0.01)
+    assert_in_delta(89.087927, results["Feet"], 0.01)
+    assert_in_delta(1069.055, results["Inches"], 0.01)
+    assert_in_delta(2715.4, results["Centimeters"], 0.01)
+    assert_in_delta(27154, results["Millimeters"], 0.01)
+    assert_in_delta(0.014661987, results["Nautical Miles"], 0.01)
   end
 
   def test_volume_conversion
-    post :perform_conversion, :input => "US Pint 65"
+    post :perform_conversion, :input => "US Pints 65"
     results = assigns["results"]
-    assert_not_nil results, "Conversion with input: \"US Pint 65\" failed"    
-    assert_in_delta(30.756471, results["Liter"], 0.01)
-    assert_in_delta(8.125, results["US Gallon"], 0.01)
-    assert_in_delta(0.030756471, results["Cubic Meter"], 0.01)    
+    assert_not_nil results, "Conversion with input: \"US Pints 65\" failed"    
+    assert_in_delta(30.756471, results["Liters"], 0.01)
+    assert_in_delta(8.125, results["US Gallons"], 0.01)
+    assert_in_delta(0.030756471, results["Cubic Meters"], 0.01)    
   end
 
   def test_speed_conversion
-    post :perform_conversion, :input => "268 Kilometers/h"
+    post :perform_conversion, :input => "268 Kilometers / Hour"
     results = assigns["results"]
-    assert_not_nil results, "Conversion with input: \"268 Kilometers/h\" failed"
+    assert_not_nil results, "Conversion with input: \"268 Kilometers / Hour\" failed"
     assert_in_delta(144.70842, results["Knots"], 0.01)
-    assert_in_delta(74.444444, results["Meters/s"], 0.01)
-    assert_in_delta(166.52748, results["Miles/h"], 0.01)
-    assert_in_delta(4.4666667, results["Kilometers/minute"], 0.01)  
+    assert_in_delta(74.444444, results["Meters / Second"], 0.01)
+    assert_in_delta(166.52748, results["Miles / Hour"], 0.01)
+    assert_in_delta(4.4666667, results["Kilometers / Minute"], 0.01)  
   end
 
 
